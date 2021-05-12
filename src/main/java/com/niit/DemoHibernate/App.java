76fb70b0d9a0c31c1d9f1500771bf2621b2eb077
Aryan Akshay <varyanakshay@gmail.com>
2021-05-12 14:32:26 +0530

    Initial commit

---
+++ src/main/java/com/niit/DemoHibernate/App.java
@@ -0,0 +1,30 @@
+package com.niit.DemoHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Student student = new Student(21, "abhiiiii", 7,99);
+        
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        session.save(student);
+        transaction.commit();
+      
+    }
+}
